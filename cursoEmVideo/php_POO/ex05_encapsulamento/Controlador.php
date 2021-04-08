75c37faeaf143dc53274fffecf27fe24bdc1d29a
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-04-08 10:38:49 -0300

    add interface

---
+++ cursoEmVideo/php_POO/ex05_encapsulamento/Controlador.php
@@ -0,0 +1,14 @@
+<?php
+// Os métods são public e já abstract por padrão;
+interface Controlador {
+    public function ligar();
+    public function desligar();
+    public function abrirMenu();
+    public function fecharMenu();
+    public function maisVolume();
+    public function menosVolume();
+    public function ligarMudo();
+    public function desligarMudo();
+    public function play();
+    public function pause();
+}
