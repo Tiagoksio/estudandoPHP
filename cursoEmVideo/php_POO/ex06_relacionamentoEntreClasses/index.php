e257389bea2650487aab8863dc5a466c74e29052
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-04-09 16:39:34 -0300

    teste de relação entre as classes lutador e luta

--- cursoEmVideo/php_POO/ex06_relacionamentoEntreClasses/index.php
+++ cursoEmVideo/php_POO/ex06_relacionamentoEntreClasses/index.php
@@ -12,16 +12,22 @@
     <pre>
     <?php
     require_once "Lutador.php";
+    require_once "Luta.php";
     $campeoes = array();
-    $campeoes[] = new Lutador("Pretty Boy", "França", 30, 1.75, 90.9, 11, 2, 1);
-    $campeoes[] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
-    $campeoes[] = new Lutador("SnapShadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
-    $campeoes[] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
-    $campeoes[] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
-    $campeoes[] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
-    print_r($campeoes);
+    $campeoes[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 90.9, 11, 2, 1);
+    $campeoes[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+    $campeoes[2] = new Lutador("SnapShadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+    $campeoes[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+    $campeoes[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+    $campeoes[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+    
+    $UEC01 = new Luta();
+    $UEC01 -> marcarLuta($campeoes[2], $campeoes[5]);
+    $UEC01 -> lutar();
+    $campeoes[2] -> status();
+    echo "\n";
+    $campeoes[5] -> status();
 
-    $campeoes[3] -> apresentar();
     ?>
     </pre>
 </body>
