0716463b98065a3a6932273da80f3855545e8033
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-04-13 23:47:22 -0300

    add superclass

---
+++ cursoEmVideo/php_POO/ex08_heranca/Pessoa.php
@@ -0,0 +1,30 @@
+<?php
+class Pessoa {
+    //Atributos
+    private $nome;
+    private $idade;
+    private $sexo;
+    //Métodos
+    public function fazerAniv() {
+        $this -> setIdade($this -> getIdade() +1);
+    }
+    //Métodos especiais
+    public function getNome() {
+        return $this -> nome;
+    }
+    public function setNome($nome) {
+        $this -> nome = $nome;
+    }
+    public function getIdade() {
+        return $this -> idade;
+    }
+    public function setIdade($idade) {
+        $this -> idade = $idade;
+    }
+    public function getSexo() {
+        return $this -> sexo;
+    }
+    public function setSexo($sexo) {
+        $this -> sexo = $sexo;
+    }
+}
